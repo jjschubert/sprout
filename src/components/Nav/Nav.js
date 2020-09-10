9b9d40fb0bd03fd91d4eb3e6c7bf55e259e04933
Jenni Schubert <jenni.schubert@gmail.com>
2020-09-10 15:38:16 -0500

    save reducer updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
@@ -13,6 +13,8 @@ const Nav = (props) => {
     path: '/login',
     text: 'Login / Register',
   };
+
+  
 
   if (props.store.user.id != null) {
     loginLinkData.path = '/dashboard';
